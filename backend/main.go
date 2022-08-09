d95deedfaf20d2e682afaa983d5f7a7d46821578
loudyowen <loudyowen027@gmail.com>
2022-08-09 18:31:34 +0700

    dev1

--- backend/main.go
+++ backend/main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+func formPost(c *gin.Context) {
+	c.Request.ParseForm()
+	for key, val := range c.Request.PostForm {
+		fmt.Println(key, val)
+	}
+}
+
 func main() {
 	var r *gin.Engine
 	r = gin.Default()
-	r.LoadHTMLGlob("views/*")
-	r.GET("/home", func(ctx *gin.Context) {
-		ctx.HTML(
+	// r.LoadHTMLGlob("views/*")
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(
 			http.StatusOK,
-			"home.html",
 			gin.H{
-				"title": "Home Page",
+				"data": "It Works!",
 			},
 		)
 	})
+	r.POST("/", formPost)
 	r.Run(":5000")
 }
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: "http://127.0.0.1:80"})
+const API = axios.create({baseURL: "http://127.0.0.1:5000"})
 
-export const sendForm = () => API.post('/formPost')
+export const sendForm = () => API.post('/')
